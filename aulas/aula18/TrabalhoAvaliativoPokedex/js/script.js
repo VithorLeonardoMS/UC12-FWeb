a02cdabf5a000570974b034606a74c4bca1e8270
VithorLeonardoMS <vithorleonardomellosoares@gmail.com>
2025-05-09 16:44:00 -0300

    Pokedex com botoes laterais

--- aulas/aula18/TrabalhoAvaliativoPokedex/js/script.js
+++ aulas/aula18/TrabalhoAvaliativoPokedex/js/script.js
@@ -24,53 +24,55 @@ let back = document.querySelector("#btnVoltar")
 
 let next = document.querySelector("#btnProximo")
 
-let numeroPokedex = 1;
+let btn1 = document.querySelector("#btn1");
+
+let btn2 = document.querySelector("#btn2");
+
+let btnQ = document.querySelector("#btnQ");
+
+let btnE = document.querySelector("#btnE");
+
+let btnBack = document.querySelector("#btnBack");
+
+let btnNext = document.querySelector("#btnNext");
 
 let somFundo = document.querySelector("#audioFundo")
 somFundo.volume = 0.1;
 
-function defineImagemPokemon(url1, url2){
-    imgPokemon.src = url1;
-  
-    imgPokemon.onerror = function () {
-      this.onerror = null; // Evita loop
-      this.src = url2;
-    };
+/*----------------Váriaveis auxiliares*/
 
-    return;
+let numeroPokedex = 1;
+
+
+/*----------------Funções auxiliares------------------ */
+
+/**
+ * Função que define a imagem do pokemon, e que em caso de erro, 
+ * define uma segunda ou até terceira url.
+ * @param {string} url1 
+ * @param {string} url2 
+ * @param {string} url3 //Opcional
+ */
+function defineImagemPokemon(url1, url2, url3 = false){
+    imgPokemon.src = url1;
+    imgPokemon.onerror = function () {
+        if(url3){
+            this.onerror = () => {
+                this.src = url3
+            }
+        }else{
+            this.onerror = null; // Evita loop
+        }
+        
+        this.src = url2;
+    };
 }
 
 function tocarSom(som){
     som = new Audio(som);
-    som.volume = 0.4
+    som.volume = 0.3
     som.play();
 }
-
-const fetchPokemon = async(pokemon) => {
-    const APIresponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
-    const data = await APIresponse.json();
-    return data;
-};
-
-const showPokemon = async(pokemon) => {
-    const dataPokemon = await fetchPokemon(pokemon);
-    defineImagemPokemon(dataPokemon.sprites.other.showdown.front_default,dataPokemon.sprites.front_default);
-
-    nomePoke.innerHTML = dataPokemon.name;
-    idPoke.innerHTML = dataPokemon.id + " | ";
-    tipo1Poke.innerHTML = dataPokemon.types[0].type.name + " | ";
-    tipo2Poke.innerHTML = dataPokemon.types[1].type.name;
-    habilidade.innerHTML = dataPokemon.abilities[0].ability.name;
-    pesoPoke.innerHTML = (dataPokemon.weight / 10).toFixed(2) + " kg";
-    altura.innerHTML = (dataPokemon.height / 10).toFixed(2) + " cm";
-    tocarSom(dataPokemon.cries.latest);
-};
-
-formPoke.addEventListener("submit", (event) => {
-    event.preventDefault();
-    numeroPokedex = parseInt(inputF.value)
-    showPokemon(inputF.value.toLowerCase());
-});
 
 function reduzirNumPokedex(){
     if(numeroPokedex > 1){
@@ -86,6 +88,45 @@ function aumentarNumPokedex(){
     showPokemon(numeroPokedex);
 }
 
+/**
+ * Toca som **legacy** do pokemon.
+ * @param {number} pokemon 
+ */
+const tocarSom1 = async(pokemon) => {
+    const dataPokemon = await fetchPokemon(pokemon);
+    tocarSom(dataPokemon.cries.latest)
+};
+
+
+/**
+ * Toca som **legacy** do pokemon.
+ * @param {number} pokemon 
+ */
+const tocarSom2 = async(pokemon) => {
+    const dataPokemon = await fetchPokemon(pokemon);
+    tocarSom(dataPokemon.cries.legacy);
+};
+
+/**
+ * Transforma a imagem do pokemon para o tipo **shine**.
+ * @param {number} pokemon 
+ */
+const shine = async(pokemon) => {
+    const dataPokemon = await fetchPokemon(pokemon);
+    defineImagemPokemon(dataPokemon.sprites.other.showdown.front_shiny,dataPokemon.sprites.front_shiny)
+};
+
+/**
+ * Retorna a imgame do pokemon para a forma basica
+ * @param {number} pokemon 
+ */
+const formaBasica = async(pokemon) => {
+    const dataPokemon = await fetchPokemon(pokemon);
+    defineImagemPokemon(dataPokemon.sprites.other.showdown.front_default,dataPokemon.sprites.front_default);
+};
+
+/* ----------------Botões-----------------*/
+
 back.addEventListener("click", (event) => {
     reduzirNumPokedex()
 });
@@ -94,29 +135,31 @@ next.addEventListener("click", (event) => {
     aumentarNumPokedex();
 });
 
-ImageBitmapRenderingContext;
+btn1.addEventListener("click", (event) => {
+    tocarSom1(numeroPokedex);
+});
 
-showPokemon(numeroPokedex);
+btn2.addEventListener("click", (event) => {
+    tocarSom2(numeroPokedex);
+});
 
-const tocarSom1 = async(pokemon) => {
-    const dataPokemon = await fetchPokemon(pokemon);
-    tocarSom(dataPokemon.cries.latest)
-};
+btnQ.addEventListener("click", (event) => {
+    shine(numeroPokedex)
+});
 
-const tocarSom2 = async(pokemon) => {
-    const dataPokemon = await fetchPokemon(pokemon);
-    tocarSom(dataPokemon.cries.legacy);
-};
+btnE.addEventListener("click", (event) => {
+    formaBasica(numeroPokedex)
+});
 
-const shine = async(pokemon) => {
-    const dataPokemon = await fetchPokemon(pokemon);
-    defineImagemPokemon(dataPokemon.sprites.other.showdown.front_shiny,dataPokemon.sprites.front_shiny)
-};
+btnBack.addEventListener("click", (event) => {
+    reduzirNumPokedex()
+});
 
-const formaBase = async(pokemon) => {
-    const dataPokemon = await fetchPokemon(pokemon);
-    defineImagemPokemon(dataPokemon.sprites.other.showdown.front_default,dataPokemon.sprites.front_default);
-};
+btnNext.addEventListener("click", (event) => {
+    aumentarNumPokedex()
+});
+
+/*-------------------Teclas------------------ */
 
 document.addEventListener('keydown', function(event) {
     switch(event.key) {
@@ -136,8 +179,38 @@ document.addEventListener('keydown', function(event) {
             shine(numeroPokedex)
             break;
         case "e":
-            formaBase(numeroPokedex)
+            formaBasica(numeroPokedex)
             break;
     }
 })
 
+/* ------------------Vizualização--------------- */
+
+const fetchPokemon = async(pokemon) => {
+    const APIresponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+    const data = await APIresponse.json();
+    return data;
+};
+
+const showPokemon = async(pokemon) => {
+    const dataPokemon = await fetchPokemon(pokemon);
+    defineImagemPokemon(dataPokemon.sprites.other.showdown.front_default,dataPokemon.sprites.front_default);
+    nomePoke.innerHTML = dataPokemon.name;
+    inputF.value = dataPokemon.id;
+    tipo1Poke.innerHTML = dataPokemon.types[0].type.name + " | ";
+    tipo2Poke.innerHTML = dataPokemon.types[1].type.name;
+    habilidade.innerHTML = dataPokemon.abilities[0].ability.name;
+    pesoPoke.innerHTML = (dataPokemon.weight / 10).toFixed(2) + " kg";
+    altura.innerHTML = (dataPokemon.height / 10).toFixed(2) + " cm";
+    //tocarSom(dataPokemon.cries.latest);
+};
+
+formPoke.addEventListener("submit", (event) => {
+    event.preventDefault();
+    numeroPokedex = parseInt(inputF.value)
+    showPokemon(inputF.value.toLowerCase());
+});
+
+ImageBitmapRenderingContext;
+
+showPokemon(numeroPokedex);
